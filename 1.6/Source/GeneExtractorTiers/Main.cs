1f2a9b08d1779aaceb6c54e7b03e1bbd7d190238
RedMattis <redmattis@gmail.com>
2025-06-18 22:08:55 +0200

    1.6 unstable update

---
+++ 1.6/Source/GeneExtractorTiers/Main.cs
@@ -0,0 +1,84 @@
+﻿
+using Verse;
+using HarmonyLib;
+using UnityEngine;
+//using VariedBodySizes;
+
+namespace GeneExtractorTiers
+{
+    /// <summary>
+    /// Better Children Skill Learning multiplies XP by 3 for children.
+    /// </summary>
+    [StaticConstructorOnStartup]
+    internal class GeneExtractorMain : Mod
+    {
+        public static GeneExtractorMain instance = null;
+        public static ExtractorTierSettings settings;
+
+        private string extractionBuff, multipakBuff, megaPackBuff, splitZeroCost, geneRegrowBuff, nutritionUse;
+        public GeneExtractorMain(ModContentPack content) : base(content)
+        {
+            instance = this;
+            settings = GetSettings<ExtractorTierSettings>();
+
+            ApplyHarmonyPatches();
+        }
+
+        static void ApplyHarmonyPatches()
+        {
+            var harmony = new Harmony("RedMattis.GeneExtractorTiers");
+            harmony.PatchAll();
+
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listStd = new Listing_Standard();
+
+
+            listStd.Begin(inRect);
+            
+            listStd.TextFieldNumericLabeled("GET_MultipackChance".Translate(), ref settings.multipackChance, ref multipakBuff, min: 0.0f, max: 1.0f);
+            listStd.TextFieldNumericLabeled("GET_MegapackChance".Translate(), ref settings.megaMultipackChance, ref megaPackBuff, min: 0.0f, max: 1.0f);
+            listStd.TextFieldNumericLabeled("GET_SplitZeroCostChance".Translate(), ref settings.splitZeroCost, ref splitZeroCost, min: 0.0f, max: 1.0f);
+
+            listStd.TextFieldNumericLabeled("GET_ExtractionTime".Translate(), ref settings.extractionHours, ref extractionBuff, min: 0.1f, max: 960);
+            listStd.TextFieldNumericLabeled("GET_NutritionUseMultiplier".Translate(), ref settings.nutritionMultiplier, ref nutritionUse, min: 0.001f, max: 10);
+            listStd.TextFieldNumericLabeled("GET_GenesRegrowingTime".Translate(), ref settings.genesRegrowingHours, ref geneRegrowBuff, min: -1f, max: 960f);
+
+            listStd.CheckboxLabeled("GET_AllVatsCanTargetGenes".Translate(), ref settings.allVatsCanTargetGenes);
+            listStd.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "GET_GeneExtractorTiers".Translate();
+        }
+    }
+
+    public class ExtractorTierSettings: ModSettings
+    {
+        public float extractionHours = 192;
+        public float multipackChance = 0.40f;
+        public float megaMultipackChance = 0.10f;
+        public float splitZeroCost = 1f;
+        public float genesRegrowingHours = -1f;
+        public float nutritionMultiplier = 1f;
+        public bool allVatsCanTargetGenes = false;
+        
+        public int RegrowTimeInTicks => (int)genesRegrowingHours * 2500;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref extractionHours, "extractionTime", 192);
+            Scribe_Values.Look(ref multipackChance, "multipackChance", 0.40f);
+            Scribe_Values.Look(ref megaMultipackChance, "megaMultipackChance", 0.10f);
+            Scribe_Values.Look(ref splitZeroCost, "splitZeroCost", 1f);
+            Scribe_Values.Look(ref nutritionMultiplier, "nutritionMultiplier", 1);
+            Scribe_Values.Look(ref genesRegrowingHours, "genesRegrowingHours", -1f);
+            Scribe_Values.Look(ref allVatsCanTargetGenes, "allVatsCanTargetGenes", false);
+            base.ExposeData();
+        }
+    }
+}
---
+++ 1.6/Source/GeneExtractorTiers/WorkGivers.cs
@@ -0,0 +1,157 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+namespace GeneExtractorTiers
+{
+	public class WorkGiver_HaulToGrowthGeneExtractor_II : WorkGiver_Scanner
+	{
+		private const float NutritionBuffer = 2.5f;
+		public override ThingRequest PotentialWorkThingRequest => ThingRequest.ForDef(DefOfs.GET_GeneExtractor_II);
+		public override PathEndMode PathEndMode => PathEndMode.Touch;
+
+		public override Job JobOnThing(Pawn pawn, Thing t, bool forced = false)
+		{
+            if (t is not Building_GeneExtractorTier geneVat)
+            {
+                return null;
+            }
+
+            //Building_GeneExtractorTier geneVat = (Building_GeneExtractorTier)t;
+			if (geneVat.NutritionNeeded > 0f)
+			{
+				ThingCount thingCount = FindNutrition(pawn, geneVat);
+				if (thingCount.Thing != null)
+				{
+					Job job = HaulAIUtility.HaulToContainerJob(pawn, thingCount.Thing, t);
+					job.count = Mathf.Min(job.count, thingCount.Count);
+					return job;
+				}
+			}
+			return null;
+		}
+
+		public override bool HasJobOnThing(Pawn pawn, Thing t, bool forced = false)
+		{
+            if (t.IsForbidden(pawn) || !pawn.CanReserve(t, 1, -1, null, forced))
+            {
+                return false;
+            }
+            if (pawn.Map.designationManager.DesignationOn(t, DesignationDefOf.Deconstruct) != null)
+			{
+				return false;
+			}
+			if (t.IsBurning())
+			{
+				return false;
+			}
+            if (t is not Building_GeneExtractorTier geneVat)
+            {
+                return false;
+            }
+            //Building_GeneExtractorTier geneVat = (Building_GeneExtractorTier)t;
+			if (geneVat.NutritionNeeded > 2.5f)
+			{
+				if (FindNutrition(pawn, geneVat).Thing == null)
+				{
+					JobFailReason.Is("NoFood".Translate());
+					return false;
+				}
+				return true;
+			}
+			return false;
+		}
+
+		private ThingCount FindNutrition(Pawn pawn, Building_GeneExtractorTier vat)
+		{
+			Thing thing = GenClosest.ClosestThingReachable(pawn.Position, pawn.Map, ThingRequest.ForGroup(ThingRequestGroup.FoodSourceNotPlantOrTree), PathEndMode.ClosestTouch, TraverseParms.For(pawn), 9999f, Validator);
+			if (thing == null)
+			{
+				return default(ThingCount);
+			}
+			int b = Mathf.CeilToInt(vat.NutritionNeeded / thing.GetStatValue(StatDefOf.Nutrition));
+			return new ThingCount(thing, Mathf.Min(thing.stackCount, b));
+			bool Validator(Thing x)
+			{
+				if (x.IsForbidden(pawn) || !pawn.CanReserve(x))
+				{
+					return false;
+				}
+				if (!vat.CanAcceptNutrition(x))
+				{
+					return false;
+				}
+				if (x.def.GetStatValueAbstract(StatDefOf.Nutrition) > vat.NutritionNeeded)
+				{
+					return false;
+				}
+				return true;
+			}
+		}
+	}
+
+	//public class WorkGiver_HaulToGrowthGeneExtractor_II : WorkGiver_HaulToGrowthGeneExtractor_II
+	//{
+	//	public override ThingRequest PotentialWorkThingRequest => ThingRequest.ForDef(DefOfs.GET_GeneExtractor_III);
+	//}
+
+
+	public class WorkGiver_CarryToGeneExtractor_II : WorkGiver_CarryToBuilding
+	{
+		public override ThingRequest PotentialWorkThingRequest => ThingRequest.ForDef(DefOfs.GET_GeneExtractor_II);
+
+		public override bool ShouldSkip(Pawn pawn, bool forced = false)
+		{
+			if (!base.ShouldSkip(pawn, forced))
+			{
+				return !ModsConfig.BiotechActive;
+			}
+			return true;
+		}
+	}
+
+    public class WorkGiver_HaulToGrowthGeneExtractor_III : WorkGiver_HaulToGrowthGeneExtractor_II
+    {
+        public override ThingRequest PotentialWorkThingRequest => ThingRequest.ForDef(DefOfs.GET_GeneExtractor_III);
+    }
+
+    public class WorkGiver_CarryToGeneExtractor_III : WorkGiver_CarryToBuilding
+	{
+		public override ThingRequest PotentialWorkThingRequest => ThingRequest.ForDef(DefOfs.GET_GeneExtractor_III);
+
+		public override bool ShouldSkip(Pawn pawn, bool forced = false)
+		{
+			if (!base.ShouldSkip(pawn, forced))
+			{
+				return !ModsConfig.BiotechActive;
+			}
+			return true;
+		}
+	}
+
+    public class WorkGiver_HaulToGrowthGeneExtractor_IV : WorkGiver_HaulToGrowthGeneExtractor_II
+    {
+        public override ThingRequest PotentialWorkThingRequest => ThingRequest.ForDef(DefOfs.GET_GeneExtractor_IV);
+    }
+
+    public class WorkGiver_CarryToGeneExtractor_IV : WorkGiver_CarryToBuilding
+    {
+        public override ThingRequest PotentialWorkThingRequest => ThingRequest.ForDef(DefOfs.GET_GeneExtractor_IV);
+
+        public override bool ShouldSkip(Pawn pawn, bool forced = false)
+        {
+            if (!base.ShouldSkip(pawn, forced))
+            {
+                return !ModsConfig.BiotechActive;
+            }
+            return true;
+        }
+    }
+
+}
