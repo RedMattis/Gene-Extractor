0b2b043c3d765a731ce9d6149fa097fde07b2136
Erik Frantz <bardez@bardez.net>
2025-11-13 23:35:31 -0600

    Move to a child namespace

---
+++ 1.6/Source/GeneExtractorTiers/Extractors/GeneExtractorTiers.cs
@@ -0,0 +1,35 @@
+﻿using RimWorld;
+using Verse;
+
+namespace GeneExtractorTiers.Extractors
+{
+    [StaticConstructorOnStartup]
+    public class Build_GeneExtractorTier_II : Building_GeneExtractorTier
+    {
+    }
+
+    [StaticConstructorOnStartup]
+    public class Build_GeneExtractorTier_III : Building_GeneExtractorTier
+    {
+        public override bool CanExtractArchite => true;
+        public override float SpeedMultiplier => 1.25f;
+    }
+
+    [StaticConstructorOnStartup]
+    public class Build_GeneExtractorTier_IV : Building_GeneExtractorTier
+    {
+        public override bool CanExtractArchite => true;
+        public override bool CanTargetExtraction => true;
+        public override float SpeedMultiplier => 2.5f;
+    }
+
+
+    [DefOf]
+    public static class DefOfs
+    {
+        public static ThingDef GET_GeneExtractor_II;
+        public static ThingDef GET_GeneExtractor_III;
+        public static ThingDef GET_GeneExtractor_IV;
+    }
+
+}
