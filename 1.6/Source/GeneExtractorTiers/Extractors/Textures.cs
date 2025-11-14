377b05cd66b490889a2d43144a406a63fff5b9a5
Erik Frantz <bardez@bardez.net>
2025-11-14 02:01:21 -0600

    Extracting the textures to a static class

---
+++ 1.6/Source/GeneExtractorTiers/Extractors/Textures.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace GeneExtractorTiers.Extractors
+{
+    public static class Textures
+    {
+        public static readonly Texture2D CancelLoadingIcon = ContentFinder<Texture2D>.Get("UI/Designators/Cancel");
+        public static readonly Texture2D CancelIcon = CancelLoadingIcon;
+        public static readonly Texture2D ActivateOverdrive = ContentFinder<Texture2D>.Get("GET_OverdriveOn");
+        public static readonly Texture2D CancelOverdrive = ContentFinder<Texture2D>.Get("GET_OverdriveOff");
+        public static readonly Texture2D TargetGeneIcon = ContentFinder<Texture2D>.Get("GET_TargetGene");
+        public static readonly Texture2D InsertPawn = ContentFinder<Texture2D>.Get("UI/Gizmos/InsertPawn");
+    }
+}
